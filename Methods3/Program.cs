4ffe0f3cf67c06a4b95ccc7a676e71ef8ffb6692
Spener-Chapman <spencerchap@hotmail.com>
2021-09-27 13:23:46 -0300

    created the loop that crashes as example

--- Methods3/Program.cs
+++ Methods3/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Methods3
 {
+    
     class Program
     {
+        static int count = 0;
+        static void Loop()
+        {
+            count = count + 1;
+            Console.WriteLine("this is loop" + count);
+            Loop(); // creates infiite loop of death
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("methods3");
+            
+            Loop();
+
+            Console.ReadKey(true);
         }
     }
 }
